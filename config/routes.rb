aee9b592d3e1514bdaf2b40b337c92d7fc1b81e7
jiemeechong <jamiecjm@gmail.com>
2018-03-05 21:59:31 +0800

    Redirect to root if not path found

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'dashboard#index'
+
+  get '*path' => redirect('/')
 end
