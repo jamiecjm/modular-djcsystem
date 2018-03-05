7a7680de7eb1acff5a1f723ba2a465fcfa672cc6
jiemeechong <jamiecjm@gmail.com>
2018-03-06 00:56:11 +0800

    Fix chart take 4

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,19 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = proc{ current_website.email }
+  config.mailer_sender = proc{ 
+    host = ActionMailer::Base.default_url_options[:host]
+      subdomain = ActionMailer::Base.default_url_options[:host].split('.')[0]
+    website = Website.find_by(external_host: host)
+    if website.nil?
+      if subdomain == 'www'
+          return nil
+      else
+          website = Website.find_by(subdomain: subdomain)
+      end
+    end  
+    website.email
+  }
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
