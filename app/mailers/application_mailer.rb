f00dc313946e13ca60a0390612b1b85503d227d4
jiemeechong <jamiecjm@gmail.com>
2018-03-06 01:31:22 +0800

    Change all default from to no-reply

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,16 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: proc{
-	host = ActionMailer::Base.default_url_options[:host]
-  	subdomain = ActionMailer::Base.default_url_options[:host].split('.')[0]
-	website = Website.find_by(external_host: host)
-	if website.nil?
-		if subdomain == 'www'
-		  	return nil
-		else
-		  	website = Website.find_by(subdomain: subdomain)
-		end
-	end  
-	website.email
-  }
+  default from: 'no-reply@djcsystem.com'
   layout 'mailer'
 end
