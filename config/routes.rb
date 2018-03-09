ac693cb7dd3f033ead8fc3f1d44d4ac3b5a00f72
jiemeechong <jamiecjm@gmail.com>
2018-03-09 21:47:40 +0800

    Add subdomain www

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
+require 'subdomain'
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	constraints(Subdomain) do
+	  devise_for :users, ActiveAdmin::Devise.config
+	  ActiveAdmin.routes(self)
+	  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'dashboard#index'
+	  root 'dashboard#index'
 
-  get '*path' => redirect('/')
+	  get '*path' => redirect('/')
+	end
+
+	constraints subdomain: 'www' do
+		root 'pages#main'
+		get '*path' => redirect('/')
+	end
+
 end
