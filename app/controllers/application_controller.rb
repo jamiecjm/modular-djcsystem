3aa9e568c62bd8efbe884a64b830372b15b45836
jiemeechong <jamiecjm@gmail.com>
2018-03-11 20:17:02 +0800

    Set raven user context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < ActionController::Base
 	private
 
 	def set_raven_context
-		Raven.user_context(id: session[:current_user_id]) # or anything else in session
+		Raven.user_context(id: current_user.id, email: current_user.email) # or anything else in session
 		Raven.extra_context(params: params.to_unsafe_h, url: request.url)
 	end
 end
