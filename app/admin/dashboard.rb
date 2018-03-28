81909402a75b670d0b9bc5cbd7c3b8a06992d1fb
jiemeechong <jamiecjm@gmail.com>
2018-03-28 18:29:55 +0800

    Change theme

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,37 +4,55 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
     columns do
-        column do
-            panel 'Quick Links', id: 'quick-links' do
-                a href: new_sale_path, class: 'card' do
-                    span class: 'card-content' do
-                        'New Sale'
-                    end
-                end
-                a href: sales_path, class: 'card' do
-                    span class: 'card-content' do
-                        'Team Sales'
-                    end
-                end
-                a href: salevalues_path, class: 'card' do
-                    span class: 'card-content' do
-                        'Individual Sales'
-                    end
-                end
-                a href: teams_path, class: 'card' do
-                    span class: 'card-content' do
-                        'Team Performance'
-                    end
-                end
-                a href: users_path, class: 'card' do
-                    span class: 'card-content' do
-                        'Team members'
-                    end
+        panel 'Quick Links', id: 'quick-links' do
+            a href: new_sale_path, class: 'card' do
+                span class: 'card-content' do
+                    'New Sale'
                 end
             end
+            a href: sales_path, class: 'card' do
+                span class: 'card-content' do
+                    'Team Sales'
+                end
+            end
+            a href: salevalues_path, class: 'card' do
+                span class: 'card-content' do
+                    'Individual Sales'
+                end
+            end
+            a href: teams_path, class: 'card' do
+                span class: 'card-content' do
+                    'Team Performance'
+                end
+            end
+            a href: users_path, class: 'card' do
+                span class: 'card-content' do
+                    'Team members'
+                end
+            end
+            if current_user.admin?
+                a href: new_project_path, class: 'card' do
+                    span class: 'card-content' do
+                        'New Project'
+                    end
+                end
+                a href: projects_path, class: 'card' do
+                    span class: 'card-content' do
+                        'Project Lists'
+                    end
+                end
+                a href: positions_path, class: 'card' do
+                    span class: 'card-content' do
+                        'Positions'
+                    end
+                end
+                a href: company_profiles_path, class: 'card' do
+                    span class: 'card-content' do
+                        'Company Profile'
+                    end
+                end                                
+            end
         end
-
-        column
     end
   end # content
 end
