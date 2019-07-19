1e77099c081b39480529ba00e9c5240ceb668419
Jie Mee Chong <jamiecjm@gmail.com>
2019-07-19 22:36:16 +1000

    remove from

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,49 +1,45 @@
 class UserMailer < ApplicationMailer
-
-  def approve_registration(user,website)
-    @user     = user
+  def approve_registration(user, website)
+    @user = user
     @company_name = website.superteam_name
     subject = "DJC Sales System: Approval Needed for Account Registration"
     recipients = User.where(admin: true).pluck(:email)
-    mail( :to      => recipients,
-          :subject => subject) do |format|
-            format.html
-	  end
-	end
-
-  def notify(users,website)
-    @users = users
-    @company_name = website.superteam_name
-    subject = "DJC Sales System: Account Registration Approved"
-    mail( :to      => @users.pluck(:email),
-          :subject => subject) do |format|
-            format.html
+    mail(:to => recipients,
+         :subject => subject) do |format|
+      format.html
     end
   end
 
-	def generate_report(sale, website)
-	    @sale = sale
-      @company_name = website.superteam_name
-	    mail( :to      => sale.users.pluck(:email),
-	          :subject => "Sale Report \##{@sale.id}") do |format|
-	            format.html
-		end
-	end
+  def notify(users, website)
+    @users = users
+    @company_name = website.superteam_name
+    subject = "DJC Sales System: Account Registration Approved"
+    mail(:to => @users.pluck(:email),
+         :subject => subject) do |format|
+      format.html
+    end
+  end
 
-  def email_admin(var={})
+  def generate_report(sale, website)
+    @sale = sale
+    @company_name = website.superteam_name
+    mail(:to => sale.users.pluck(:email),
+         :subject => "Sale Report \##{@sale.id}") do |format|
+      format.html
+    end
+  end
+
+  def email_admin(var = {})
     @user = var[:user]
     @sale = var[:sale]
     @company_name = var[:company].superteam_name
     @content = var[:content]
-    mail( :to => var[:to],
-          :cc => var[:cc],
-          :bcc => var[:bcc],
-          :from => @user.email,
-          :reply_to => @user.email,
-          :subject => var[:subject]) do |format|
-            format.html
+    mail(:to => var[:to],
+         :cc => var[:cc],
+         :bcc => var[:bcc],
+         :reply_to => @user.email,
+         :subject => var[:subject]) do |format|
+      format.html
     end
   end
-
-
 end
