45539155954c7bd7e08920e0f1daa0fb190a5eb1
Jie Mee Chong <jamiecjm@gmail.com>
2019-07-20 08:17:52 +1000

    fix typo

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "DJC System <no-reply@djcsystem.com>"
-  defatul return_path: ENV["DEV_EMAIL"]
+  default return_path: ENV["DEV_EMAIL"]
   layout "mailer"
 end
